ccbf72169f63cc8719cc73d10759b5ee0ac274bf
Eric Ghati <ericghati@gmail.com>
2026-06-21 21:22:34 +0300

    revert: remove backend controllers added by friend (events, match, notifications)

--- backend/routes/matchRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getMatches } = require("../controllers/matchController");
-const verifyToken = require("../middleware/auth");
-
-const router = express.Router();
-
-router.get("/", verifyToken, getMatches);
-
-module.exports = router;
